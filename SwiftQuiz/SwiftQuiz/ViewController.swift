bfe838e2bee7dca5d8d01e9865d954757075f3c0
Gabriel Henrique <gabhenrique0@gmail.com>
2019-04-24 17:26:39 -0300

    Create the initial structure of the project with the xcassets, launchscreen and main storyboard file

---
+++ SwiftQuiz/SwiftQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftQuiz
+//
+//  Created by Gabriel Henrique Santos Pereira on 24/04/19.
+//  Copyright © 2019 Gabriel Henrique Santos Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
