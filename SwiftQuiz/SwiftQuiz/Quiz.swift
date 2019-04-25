53a0b0015b16182983355427d04b4778afe94ef2
Gabriel Henrique <gabhenrique0@gmail.com>
2019-04-25 11:51:24 -0300

    Finish the implementation of swift quiz app

--- SwiftQuiz/SwiftQuiz/Quiz.swift
+++ SwiftQuiz/SwiftQuiz/Quiz.swift
@@ -11,11 +11,21 @@ import Foundation
 class Quiz {
     let question: String
     let options: [String]
-    let correctedAnswer: String
+    private let correctedAnswer: String
     
     init(question: String, options: [String], correctedAnswer: String) {
         self.question = question
         self.options = options
         self.correctedAnswer = correctedAnswer
+    }
+    
+    deinit {
+        print("Liberou o quiz da memória")
+    }
+    
+    func validateOption(_ index: Int) -> Bool {
+        let answer = options[index]
+        
+        return answer == correctedAnswer
     }
 }
--- SwiftQuiz/SwiftQuiz/ResultViewController.swift
+++ SwiftQuiz/SwiftQuiz/ResultViewController.swift
@@ -14,10 +14,22 @@ class ResultViewController: UIViewController {
     @IBOutlet weak var labelWrong: UILabel!
     @IBOutlet weak var labelScore: UILabel!
     
+    var totalCorrectAnswers: Int = 0
+    var totalAnswers: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        labelAnswered.text = "Perguntas respondidas: \(totalAnswers)"
+        labelCorrect.text = "Perguntas corretas: \(totalCorrectAnswers)"
+        labelWrong.text = "Perguntas erradas: \(totalAnswers - totalCorrectAnswers)"
+        
+        let score = totalCorrectAnswers * 100 / totalAnswers
+        
+        labelScore.text = "\(score)%"
     }
     
     @IBAction func actionClose(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
 }
